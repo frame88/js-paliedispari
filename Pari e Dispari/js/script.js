acdfb2747bf0f7f7a39973b07877d8d385bf71a6
frame88 <fleuzzi97@gmail.com>
2021-11-29 14:56:26 +0100

    schema in italiano

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,17 @@
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random(sempre da 1 a 5) per il computer
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari
+Dichiariamo chi ha vinto
+    (corrispondenza tra scelta pari e dispari e somma pari o dispari)
+In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
+*/
+
+/*
+1. sceglierer pari o dispari
+2. inserisce un numero da 1 a 5
+3. generiamo un numero random(1,5) per il pc
+4.sommiamo random a Number
+dichiariamo chi ha vinto
+*/

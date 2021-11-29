6de3cc2ff1ed0424e4867ab0d3f60b27f01763f2
frame88 <fleuzzi97@gmail.com>
2021-11-29 13:31:56 +0100

    condizione trovata

---
+++ Palidroma/js/script.js
@@ -0,0 +1,21 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+(prima senza funzione e poi con funzione)
+*/
+
+
+/*
+1. ciclo for che itera sulle lettere
+2. fino a quando la i non è == alla length della stringa
+3. se la i è uguale alla word[word.length-i], ovvero la lettera che sto considerando è uguale alla sua simmetrica
+4. allora continua (qui vorrei proprio usare il break)
+*/
+
+let word = prompt('inserisci una parola: ');
+for (let i = 0; i < word.length; i++) {
+    if (word[i] == word[word.length - (i + 1)]) {
+       console.log(word[i]);
+   }
+}
+
